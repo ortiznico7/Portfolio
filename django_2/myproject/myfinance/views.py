0e94cd0c28d701a2ab963d290d34981ee2548b56
ortiznico7 <nicholasortiz602@gmail.com>
2025-02-26 18:15:50 -0500

    Orgnanized al django files in one folder

--- django_2/myproject/myfinance/views.py
+++
@@ -1,4 +0,0 @@
-from django.views.generic import ListView
-
-class Member(ListView):
-    
--- myproject/myfinance/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Member, Account
-
-admin.site.register(Member)
-admin.site.register(Account)
-# Register your models here.
