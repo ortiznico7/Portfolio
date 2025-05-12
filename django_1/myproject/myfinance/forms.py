ee1772f47d5cd7c97e53c4f94a56ea306923cc96
ortiznico7 <nicholasortiz602@gmail.com>
2025-05-12 16:30:10 -0400

    Django2

--- django_1/myproject/myfinance/forms.py
+++ django_1/myproject/myfinance/forms.py
@@ -1,11 +1,39 @@
 from django import forms
+from django import models
 from .models import Member
+from .models import Account
 
+class Member(models.Member):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Account(models.Account):
+    title = models.CharField(max_length=100)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE, related_name='accounts')
+
+    def __str__(self):
+        return self.title
 
 class MemberForm(forms.MemberForm):
     class Meta:
         model = Member
         fields = ['firstname', 'lastname', 'member_id']
+
+        def clean_title(self):
+            title = self.cleaned_data.get('title')
+            if "bad" in title.lower():
+                raise forms.ValidationError("Inappropriate word detected in title.")
+            return title
+        
+from django import forms
+from .models import Member
+
+class AccountForm(forms.AccountForm):
+    class Meta:
+        model = Account
+        fields = ['accounttype', 'amountinacc', 'ROI']
 
         def clean_title(self):
             title = self.cleaned_data.get('title')
--- django_1/myproject/myfinance/urls.py
+++ django_1/myproject/myfinance/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import MemberList, CreateMember
 from .views import home
+from django.contrib.auth import views as auth_views
 from . import views
 from django.contrib import admin
 
@@ -9,6 +10,9 @@ urlpatterns = [
     path('create/', CreateMember.as_view(), name="member-create"),
     path('hello/', views.hello_myfin, name="hello_myfin"),
     path('', home, name='home'),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ] 
 
